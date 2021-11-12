19bc2dd4184c607fe32fa922156d7e11e373fc7d
HassanTAli <hassantarekha@gmail.com>
2021-11-12 22:20:53 +0200

    upload full Project

---
+++ pinterest/movies/api/v1/views.py
@@ -0,0 +1,80 @@
+from django.http import response
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.utils import serializer_helpers
+from movies.models import Movie
+from .serializers import MovieSerializer
+
+@api_view(['GET'])
+def hello(req,mykey):
+    data = {'message':'Hell from rest api yor key is {}'.format(mykey)}
+    if mykey == 'yes':
+        return Response(data=data,status=status.HTTP_200_OK)
+    else:
+        return Response(data=data,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def movie_list(req):
+    movies = Movie.objects.all()
+    serializer_Movie = MovieSerializer(instance=movies,many=True)
+    return Response(data=serializer_Movie.data,status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def movie_created(req):
+    serializer_movie = MovieSerializer(data=req.data)
+    if serializer_movie.is_valid():
+        serializer_movie.save()
+    else:
+        return Response(data=serializer_movie.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    data = {
+        'message':'Success',
+        'data':{"id":serializer_movie.data.get('id')}
+    }
+    # return Response(data=serializer_movie.data,status=status.HTTP_201_CREATED)
+    return Response(data=data,status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def movie_detail(req, pk):
+    movie_Obj = Movie.objects.filter(pk=pk)
+    if movie_Obj.exists():
+        pass
+    else:
+        return Response(data={'message':'failed Movie doesnot exist'},status=status.HTTP_400_BAD_REQUEST)
+        
+    serialized_Movie = MovieSerializer(instance=movie_Obj)
+    return Response(data=serialized_Movie.data,status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+def movie_delete(req,pk):
+    response = {}
+    try:
+        movie_Obj = Movie.objects.get(pk=pk)
+        movie_Obj.delete()
+        response['data']={'message':'Successfully Deleted Movie'}
+        response['status']=status.HTTP_200_OK
+    except Exception as e:
+        response['data']={'message':'Error during Deleted Movie {}'.format(str(e))}
+        response['status']=status.HTTP_400_BAD_REQUEST
+        
+    return Response(**response)
+
+@api_view(['PUT','PATCH'])
+def movie_update(req,pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Exception as e:
+        return Response(data={'message': str(e)},status=status.HTTP_400_BAD_REQUEST)
+    
+    if req.method == 'PUT':
+        serialized_movie = MovieSerializer(instance=movie,data=req.data)
+    elif req.method == 'PATCH':
+        serialized_movie = MovieSerializer(instance=movie,data=req.data,partial=True)
+        
+    if serialized_movie.is_valid():
+        serialized_movie.save()
+        return Response(data=serialized_movie.data,status=status.HTTP_200_OK)
+    return Response(data=serialized_movie.errors,status=status.HTTP_400_BAD_REQUEST)
+    
---
+++ pinterest/movies/models.py
@@ -0,0 +1,32 @@
+from abc import abstractclassmethod
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+        
+class CommonInfo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    release_date = models.DateField()
+    poster = models.ImageField(upload_to='pinterest_posters')
+    watch_count = models.IntegerField()
+    likes = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        abstract = True
+        
+class Series(CommonInfo):
+    season = models.CharField(max_length=50)
+    episode = models.CharField(max_length=50)
+
+class Movie(CommonInfo):
+    pass
